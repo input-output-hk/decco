0f1435a325ce5b4f071938281edb07904ae018c5
Shailesh Patil <shailesh.patil@iohk.io>
2019-06-27 19:07:29 +0100

    testing gpg 2

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -2,15 +2,20 @@
 
 set -eux
 
-export $gpgPrivateKey
+gpg --version
 
 echo $gpgPrivateKey | base64 --decode > gpg_key
 
-gpg --import gpg_key
+gpg --batch --passphrase "$gpgPassphrase" --import gpg_key
 
 rm gpg_key
+
+gpg --list-secret-keys
 
 mill src.io.iohk.decco.publish \
 --sonatypeCreds "$username:$password"  \
 --gpgPassphrase "$gpgPassphrase" \
+--gpgKeyName "$gpgKeyName" \
 --release false
+
+
