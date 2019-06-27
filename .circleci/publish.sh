29d34998ac758bb486672c10b6e3f944c6ea7839
Shailesh Patil <shailesh.patil@iohk.io>
2019-06-27 22:01:32 +0100

    testing gpg 2

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -5,17 +5,17 @@ set -eux
 GPG_TTY=$(tty)
 export GPG_TTY
 
-gpg2 --version
+gpg --version
 
 gpg-agent --version
 
-echo $GPG_KEY | base64 --decode > gpg_key
+echo $GPG_KEY | base64 --decode | gpg --import --batch --no-tty --yes --passphrase "$GPG_PASSPHRASE"
 
-gpg2 --batch --passphrase "$GPG_PASSPHRASE" --import --no-tty --yes gpg_key
+#gpg2 --batch --passphrase "$GPG_PASSPHRASE" --import --no-tty --yes gpg_key
 
-rm gpg_key
+#rm gpg_key
 
-gpg2 --list-secret-keys
+gpg --list-secret-keys
 
 mill -i src.io.iohk.decco.publish \
 --sonatypeCreds "$OSS_USERNAME:$OSS_PASSWORD"  \
