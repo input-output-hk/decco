d95df07751c78e4f33b7d62313d31af141c15a78
Shailesh Patil <shailesh.patil@iohk.io>
2019-06-28 14:36:54 +0100

    testing gpg 2

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -20,6 +20,8 @@ echo $GPG_KEY | base64 --decode -i | gpg --batch --import --no-tty --yes --passp
 
 gpg --list-secret-keys
 
+gpg --passphrase $GPG_PASSPHRASE --batch --yes -a -b LICENSE
+
 mill -i src.io.iohk.decco.publish \
 --sonatypeCreds "$OSS_USERNAME:$OSS_PASSWORD"  \
 --gpgPassphrase "$GPG_PASSPHRASE" \
