ab713c4ed6435b6578d8eb68224bebdad05f7ef9
Shailesh Patil <shailesh.patil@iohk.io>
2019-07-02 08:19:42 +0100

    only release snapshot from develop branch

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -2,18 +2,26 @@
 
 set -eu
 
+if [[ "$CIRCLE_BRANCH" == "develop" ]]; then
+
 echo $GPG_KEY | base64 --decode | gpg --batch --import --passphrase "$GPG_PASSPHRASE"
 
 gpg --passphrase $GPG_PASSPHRASE --batch --yes -a -b LICENSE
 
-mill -i src.io.iohk.decco.publish \
+mill src.io.iohk.decco.publish \
 --sonatypeCreds "$OSS_USERNAME:$OSS_PASSWORD"  \
 --gpgPassphrase "$GPG_PASSPHRASE" \
 --gpgKeyName "$GPG_KEY_ID" \
 --release false
 
-mill -i src.io.iohk.decco.auto.publish \
+mill src.io.iohk.decco.auto.publish \
 --sonatypeCreds "$OSS_USERNAME:$OSS_PASSWORD"  \
 --gpgPassphrase "$GPG_PASSPHRASE" \
 --gpgKeyName "$GPG_KEY_ID" \
 --release false
+
+else
+
+  echo "Skipping publish step"
+
+fi
